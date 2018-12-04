9cbc8e90ab6707a4986b0e320b7f9acd7e04abe6
Marco Trambusti <trambusti.marco23@gmail.com>
2018-12-04 09:38:20 +0100

    primo commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int iContatore=0;
+    for(int i=0; i <10; i++){iContatore++;}
+    cout<<iContatore<<endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
